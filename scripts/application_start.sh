e294ec15a67067a81f509344c876c6c0777ccea2
Vineet Bakshi <vineet.bakshi95@gmail.com>
2021-06-14 21:58:16 +0530

    Signed-off-by: Vineet Bakshi <vineet.bakshi95@gmail.com>

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,2 @@
+source /home/ec2-user/.bash_profile
 node index.js
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,4 @@
+source /home/ec2-user/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
