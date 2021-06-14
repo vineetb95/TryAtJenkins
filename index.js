f8331cc112a72474bdea7556c617abb2ab7afb6a
Vineet Bakshi <30375399+vineetb95@users.noreply.github.com>
2021-06-14 23:40:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
  res.send('This is the production environment!!');
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT ||  3000);
  
 module.exports = app;
