c3ea2899103b18a017df3175e127a4b785fc7c08
Vineet Bakshi <vineet.bakshi95@gmail.com>
2021-06-15 03:07:11 +0530

    dev one
    Signed-off-by: Vineet Bakshi <vineet.bakshi95@gmail.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('<h1>Hey does this work again and again and again ???</h1>');
+ res.send('<h1>Welcome to dev environment</h1>');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT ||  3000);
